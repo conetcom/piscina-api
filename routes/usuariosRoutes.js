ea3acbdfa51456cd8ebf72895ed366279e8a5f61
wilmer Martinez <wilmerm@conetweb.com>
2025-02-04 11:01:50 -0500

    cambio en el archivo usuariocontroller para el registro de Usuarios

--- routes/usuariosRoutes.js
+++ routes/usuariosRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const usuariosController = require('../controllers/usuariosController');
 
-router.post('/registro', usuariosController.registrarUsuario);
+router.post('/register', usuariosController.registrarUsuario);
 router.post('/login', usuariosController.loginUsuario);
 
 module.exports = router;
