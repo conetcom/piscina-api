9348628e1da7a0b2327013c53c49d7b2e6c056a6
wilmer Martinez <wilmerm@conetweb.com>
2025-02-04 17:09:51 -0500

    cambio user controler para el manejo del registro

--- models/usuariosModel.js
+++ models/usuariosModel.js
@@ -1,13 +1,13 @@
 const pool = require('./database');
 
 const getUsuarioByEmail = async (email) => {
-    const result = await pool.query('SELECT * FROM cliente WHERE email = $1', [email]);
+    const result = await pool.query('SELECT * FROM usuarios WHERE email = $1', [email]);
     return result.rows[0];
 };
 
 const createUsuario = async (username, email, hashedPassword, rol) => {
     const result = await pool.query(
-        'INSERT INTO cliente (username, email, password, rol) VALUES ($1, $2, $3, $4) RETURNING id, username, email, rol',
+        'INSERT INTO usuarios (username, email, password, rol) VALUES ($1, $2, $3, $4) RETURNING id, username, email, rol',
         [username, email, hashedPassword, rol]
     );
 
