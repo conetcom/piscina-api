6f8c70cb6a60e429d0553d3e53b42ba02ea38c56
wilmer Martinez <wilmerm@conetweb.com>
2025-02-03 22:54:13 -0500

    cambio en el archivo usuariocontroller para el registro de Usuarios

--- models/usuariosModel.js
+++ models/usuariosModel.js
@@ -1,12 +1,12 @@
 const pool = require('./database');
 
 const getUsuarioByEmail = async (email) => {
-    const result = await pool.query('SELECT * FROM cliente WHERE email = $1', [email]);
+    const result = await pool.query('SELECT * FROM usuarios WHERE email = $1', [email]);
     return result.rows[0];
 };
 
 const createUsuario = async (nombre, email, hashedPassword, rol) => {
-    await pool.query('INSERT INTO cliente (nombre, email, password, rol) VALUES ($1, $2, $3, $4)', [nombre, email, hashedPassword,rol]);
+    await pool.query('INSERT INTO usuarios (nombre, email, password, rol) VALUES ($1, $2, $3, $4)', [nombre, email, hashedPassword,rol]);
     
 };
 
