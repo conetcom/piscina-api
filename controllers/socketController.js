d7147350f5d411d3b68a9b583cb7dbb8293156f5
wilmer Martinez <wilmerm@conetweb.com>
2025-04-09 14:34:30 -0500

    modificacion controllersocket para colocar consolelog

--- controllers/socketController.js
+++ controllers/socketController.js
@@ -4,7 +4,9 @@ let users = {}; // Aquí guardamos las conexiones de usuario
 module.exports = (io) => {
     let messages = []; // Almacenamiento temporal de mensajes
     io.on('connection', (socket) => {
-        console.log('Nuevo usuario conectado');
+        
+        console.log(`[SOCKET] Mensaje recibido de ${from} para ${to}:`, content);
+  console.log(`[SOCKET] ID destino: ${targetSocketId}`);
   // Enviar mensajes anteriores al usuario nuevo
     
   socket.emit('previousMessages', messages);
@@ -12,13 +14,14 @@ module.exports = (io) => {
         // Guardar la conexión del usuario
         socket.on('register', (userId) => {
             users[userId] = socket.id; // Asociar el userId con su socket
+            
         });
 
         // Escuchar el mensaje y enviarlo al usuario destino
         socket.on('sendMessage', (message) => {
             const { to, from, content } = message;
             const targetSocketId = users[to];
-
+console.log (message);
             if (targetSocketId) {
                 // Enviar el mensaje al usuario destino
                 io.to(targetSocketId).emit('receiveMessage', { from, content });
