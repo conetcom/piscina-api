3494c3bca78e2976b0d5fd74c34b07203484d6d4
wilmer Martinez <wilmerm@conetweb.com>
2025-04-26 08:23:35 -0500

    MODIFICACION RUTAS

--- index.js
+++ index.js
@@ -38,10 +38,11 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use('/api/usuarios', usuariosRoutes);
 app.use('/api/st_piscinas', estadoPiscinas);
 app.use('/api/clientes', clientRoutes);
-app.use('/api', (req, res, next) => {
-  req.io = io; // 👈 inyectamos io en cada request
+app.use('/api/messages', (req, res, next) => {
+  req.io = io; 
   next();
 }, messageRoutes);
+
 app.use('/api/user', userRoutes);
 app.use('/api', uploadRoutes);
 
